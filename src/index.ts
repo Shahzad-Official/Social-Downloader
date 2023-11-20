e77fced08a459c63a9a0da7f88d666d99d0d0d82
Shahzad-Official <shahzad.official.90@gmail.com>
2023-11-20 20:15:17 +0500

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import downloadRouter from './routes/downloadRoutes';
 import errorHandler from './errorHandler';
 import cors from "cors";
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({
+    origin:["http://localhost:3000/"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 app.use(express.json());
+  
 app.use("/social-downloader",downloadRouter);
 
 app.use(errorHandler);
