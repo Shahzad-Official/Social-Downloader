1c9834b301b7e27232c9f534112bf723e330493b
Ubuntu <ubuntu@ip-172-31-42-188.eu-north-1.compute.internal>
2023-11-20 15:01:53 +0000

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import downloadRouter from './routes/downloadRoutes';
 import errorHandler from './errorHandler';
+import cors from "cors";
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/social-downloader",downloadRouter);
 
