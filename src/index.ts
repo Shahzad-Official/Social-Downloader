6f6e6bacc6cdea8a1fb8fe68fd31061254cb2b62
Shahzad-Official <shahzad.official.90@gmail.com>
2023-12-23 20:34:26 +0500

    updated

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ import cors from "cors";
 const app = express();
 const port = 4401;
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["http://localhost:4400","https://youtube-downloaders.com","https://www.youtube-downloaders.com"],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    }
+));
 
 app.use(express.json());
 app.use((req, res, next) => {
